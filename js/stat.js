6fa4f4b8477cac8381e9e5814c1e6caa53bd177e
Alex <aleks.bochar@gmail.com>
2019-10-03 00:52:51 +0300

    eslint bugs fixed, color detection function improved

--- js/stat.js
+++ js/stat.js
@@ -6,28 +6,25 @@ var GAP = 10;
 var COLUMN_WIDTH = 40;
 var COLUMN_HEIGHT = 50;
 var COLUMN_DIST = 50;
-var COLUMN_PositionX = CLOUD_X + COLUMN_WIDTH;
-var COLUMN_PositionY = (CLOUD_Y + GAP * 2);
-var GIST_START_Y = 83;
-var START_TEXT_Y = GIST_START_Y + 172;
+var COLUMN_POSITION_X = CLOUD_X + COLUMN_WIDTH;
 
 // Функция параметров Белого облака
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH,CLOUD_HEIGHT);
-}
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
 
 // Нахождение маскимального значения times
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
-  for (var i = 0; i < arr.lenght; i++) {
+  for (var i = 0; i < arr.length; i++) {
     if (arr[i] > maxElement) {
       maxElement = arr[i];
     }
   }
   return maxElement;
-}
+};
 
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0,0,0,0.7)');
@@ -40,36 +37,36 @@ window.renderStatistics = function (ctx, names, times) {
 
   var maxTime = getMaxElement(times);
 
-  //Цикл построения гистограммы
+  // Цикл построения гистограммы
 
   for (var i = 0; i < names.length; i++) {
     var playerName = names[i];
-    var positionX = COLUMN_PositionX + ((COLUMN_WIDTH + COLUMN_DIST) * i);
+    var positionX = COLUMN_POSITION_X + ((COLUMN_WIDTH + COLUMN_DIST) * i);
     var namePositionY = CLOUD_HEIGHT - GAP;
     ctx.fillStyle = 'black';
 
     ctx.fillText(playerName, positionX, namePositionY);
 
     var playerTime = Math.floor(times[i]);
-    var scorePositionY = CLOUD_HEIGHT - ((GAP * times[i]) / maxTime);
+    var scorePositionY = CLOUD_HEIGHT - ((COLUMN_HEIGHT * times[i]) / maxTime);
 
-    ctx.fillText(playerTime, positionX, scorePositionY);
+    ctx.fillText(playerTime, positionX, scorePositionY - 112);
 
     // Функция выбора цвета
     if (names[i] === 'Вы') {
-      ctx.fillStyle =  'rgba(255, 0, 0, 1)';
-      } else {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
       var randomNumber = Math.floor(Math.random() * 100);
 
-      ctx.fillStyle = 'hsl(200,' + randomNumber + ', 50%)';
-    };
+      ctx.fillStyle = 'hsl(240,' + randomNumber + '%, 50%)';
+    }
 
     var columnPositionY = (CLOUD_X + GAP * 2) * 2;
     var calculatedColumnHeight = -((COLUMN_HEIGHT * times[i]) / maxTime);
 
-    ctx.fillRect(positionX, columnPositionY, COLUMN_WIDTH, calculatedColumnHeight - 70);
+    ctx.fillRect(positionX, columnPositionY, COLUMN_WIDTH, calculatedColumnHeight - 80);
 
   }
-}
+};
 
 
